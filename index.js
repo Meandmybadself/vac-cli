ae2adbfd193d91b3a4fe0f2ae5dafcbc3861c1ee
Nicholas Mitrousis <nicholas.mitrousis@akqa.com>
2021-03-30 13:58:27 -0700

    feat: includes other states and ability to provide parameters as arguments

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const axios = require('axios')
 const haversine = require('haversine')
 const { execFile } = require('child_process');
+const yargs = require('yargs/yargs')
+const fs = require('fs')
 
-// Change me - Minnetonka
+const argv = yargs(process.argv.slice(2))
+    .usage('Usage example: -latlong 44.03031,-55.1100 -state CA --dist 20')
+    .demandOption(['latlong', 'state' ])
+    .argv
+
+
+const latLong = argv.latlong.split(',')
+
 // https://www.gps-coordinates.net/
 const MY_LOCATION = {
-    latitude: 44.9019313,
-    longitude: -93.468875,
+    latitude: latLong[0],
+    longitude: latLong[1],
+    state: argv.state,
+    distance: argv.dist || 20
 }
 
 const checkForSpots = async () => {
-    process.stdout.write('.')
-    const {data} = await axios.get('https://www.vaccinespotter.org/api/v0/states/MN.json')
+    const {data} = await axios.get(`https://www.vaccinespotter.org/api/v0/states/${MY_LOCATION.state}.json`)
     const features = data.features
-    .filter(({properties}) => properties.appointments_available)
     .map(feature => ({...feature, distance: haversine(MY_LOCATION, { latitude: feature.geometry.coordinates[1], longitude: feature.geometry.coordinates[0] }, {unit: 'mile'})}))
-    .filter(feature => feature.distance < 20)
-    if (features.length) {
-	if (process.platform == 'darwin') {
+    .filter(feature => feature.distance < MY_LOCATION.distance)
+
+    const locationCount = features.length
+
+    const apptAvail = features.filter(({properties}) => properties.appointments_available)
+
+    console.clear()
+    console.log(`Last check: ${new Date().toLocaleTimeString()} ${new Date().toLocaleDateString()}`)
+    console.log(`${locationCount} location(s) found within ${MY_LOCATION.distance} mile(s): ${apptAvail.length} appointment(s) available`)
+
+    if (apptAvail.length) {
+        if (process.platform == 'darwin') {
             execFile('afplay', ['horn.mp3'])
-	} else {
+        } else {
             execFile('aplay', ['horn.wav'])
-	}
-        console.log(JSON.stringify(features, null, 2))
+        }
+
+        apptAvail.forEach((appt) => {
+            console.log(JSON.stringify(appt.properties, null, 2))
+        })
+
     } else {
+        console.log(`Checking again in 10 seconds...`)
         setTimeout(checkForSpots, 10000)
     }
 }
